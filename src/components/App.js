60402c4d2289eace466a6c23c42cbd9281c5bd40
hsanch13 <hayleysanchez13@gmail.com>
2024-03-25 14:55:17 -0600

    set state to images fetched from an api

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect} from 'react'
+
+export default function App() {
+
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp) => resp.json())
+        .then((data) => {
+            setImage(data.message)
+        })
+    }, [])
+
+    if (!image) {
+        return <p>Loading... </p>
+    }
+
+  return (
+    <div>
+      <p>Cute dog photo</p>
+      <img src={image} alt="A Random Dog" ></img>
+    </div>
+  )
+}
